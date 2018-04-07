171a4376598ff584ff0f874af980d5c9182268ea
Xiu08Ting09Huang <294816902@qq.com>
2018-04-07 22:00:39 +0800

    一级菜单完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -1,3 +1,90 @@
 /**
  * Created by Damon on 2018/4/7.
  */
+
+
+$(function () {
+  var currentPage = 1;
+  var pageSize = 5;
+  //1 渲染页面
+  render();
+  function render(){
+    $.ajax({
+      url:'/category/queryTopCategoryPaging',
+      data:{
+        page:currentPage,
+        pageSize:pageSize
+      },
+      success:function (info) {
+        console.log(info);
+        $('.lt_content tbody').html(template('firstTmp',info))
+      //  分页
+        $('#paginator').bootstrapPaginator({
+          //设置版本号
+          bootstrapMajorVersion:3,
+          currentPage:info.page,
+          totalPages:Math.ceil(info.total/info.size),
+          onPageClicked:function(event, originalEvent, type,page){
+            //为按钮绑定点击事件 page:当前点击的按钮值
+            currentPage = page;
+            render();
+          }
+
+        })
+      }
+    })
+
+  }
+
+// 2  模态框
+  $('#addBtn').on('click',function () {
+    $('#addModal').modal('show');
+  })
+
+//  3 通过校验插件, 添加校验功能
+  $('#form').bootstrapValidator({
+    // 配置图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields:{
+      categoryName:{
+        validators:{
+          notEmpty:{
+            message:'请输入一级分类名称'
+          }
+        }
+      }
+    }
+  })
+
+
+  // 4 注册表单校验成功事件
+  $('#form').on('success.form.bv',function (e) {
+    e.preventDefault();
+    $.ajax({
+      url:'/category/addTopCategory',
+      type:'POST',
+      data:$('#form').serialize(),
+      success:function (info) {
+        //console.log(info);
+        if(info.success){
+          // 关闭模态框
+          $('#addModal').modal('hide');
+          currentPage = 1;
+          //重新渲染页面
+          render();
+          // 重置表单校验状态和 表单内容
+          // 传 true 不仅可以重置 状态, 还可以重置内容
+          $('#form').data('bootstrapValidator').resetForm(true)
+        }
+      }
+    })
+  })
+
+
+
+
+})
